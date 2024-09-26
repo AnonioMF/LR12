f593d6e65f09b063e1a5ba357676d37eceabd3d0
AnonioMF <sitmsnu@gmail.com>
2024-09-26 23:17:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; // Используем библиотеку Сканер
+public class Main // Обьявляем основной класс
+{
+    public static void main(String args[]) // Основной метод класса
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число для проверки:");
+
+        int num=in.nextInt(); // передаем значение переменной из системы
+        in.close(); // Закрываем ввод данных
+        int temp;
+        boolean isPrime=true;
+
+        for (int i=2; i<=num/2; i++)
+        {
+            temp = num % i;
+            if (temp == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime)
+        {
+            System.out.println(num + " - простое число");
+        } else
+        {
+            System.out.println(num + " - составное число");
+        }
+    }
+}
